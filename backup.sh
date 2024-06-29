e7c18af5ea71c18c08e23d0e98119e8c7301c943
Peter <peter.hutka2@gmail.com>
2024-06-29 13:25:04 +0200

    add setup and backup scripts

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+SETUP_DIR="$HOME/setup"
+
+cp $HOME/.xinitrc $SETUP_DIR/xinitrc
+
+cd $SETUP_DIR
+
+echo "backup done"
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+SETUP_DIR="$HOME/setup"
+
+ln -sf $SETUP_DIR/xinitrc $HOME/.xinitrc
+
+echo "setup done"
