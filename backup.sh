41a702a04d40bfb338e7aa4c604c6108571f5945
Peter <peter.hutka2@gmail.com>
2024-06-29 16:21:32 +0200

    add i3 config

--- backup.sh
+++ backup.sh
@@ -2,7 +2,9 @@
 
 SETUP_DIR="$HOME/setup"
 
-cp $HOME/.xinitrc $SETUP_DIR/xinitrc
+
+cp -r $HOME/.xinitrc $SETUP_DIR/xinitrc
+cp -r $HOME/.config/i3/config $SETUP_DIR/config/i3/config
 
 cd $SETUP_DIR
 
--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 SETUP_DIR="$HOME/setup"
 
 ln -sf $SETUP_DIR/xinitrc $HOME/.xinitrc
+ln -sf $SETUP_DIR/config/i3/config $HOME/.config/i3/config
 
 echo "setup done"
