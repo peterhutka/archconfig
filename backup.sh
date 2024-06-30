3ce86293929880189b2a80fa15f9aeabb86f9469
Peter <peter.hutka2@gmail.com>
2024-06-30 19:22:56 +0200

    add kitty config and picom

--- backup.sh
+++ backup.sh
@@ -5,6 +5,8 @@ SETUP_DIR="$HOME/setup"
 
 cp -r $HOME/.xinitrc $SETUP_DIR/xinitrc
 cp -r $HOME/.config/i3/config $SETUP_DIR/config/i3/config
+cp -r $HOME/.config/kitty/kitty.conf $SETUP_DIR/config/kitty/kitty.conf
+cp -r $HOME/.config/picom/picom.conf $SETUP_DIR/config/picom/picom.conf
 
 cd $SETUP_DIR
 
